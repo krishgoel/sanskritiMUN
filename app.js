fc1e4d3393e394fdfb5cd9379a4c5bf3d639cc29
Krish Skywalker <projectbeta@sanskritischool.edu.in>
2020-01-02 13:28:02 +0530

    Landing page commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var stylus = require('stylus');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use(stylus.middleware)
+    debug: true
+    src: __dirname + "/public/stylesheets"
+    dest: __dirname + "/public/css"
+    compile: __dirname + "/public/stylesheets"
+    // compile: (src) -> 
+    //     console.log(stylus(src))
+    //     return stylus(src)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+const port = 7500
+app.listen(port, () => console.log(`We up and running at ${port}!`))
---
+++ public/javascripts/style.js
@@ -0,0 +1,6 @@
+// Links container top-space
+var topnavHeight = document.getElementById('topnav').offsetHeight;
+// var topnavLinksContainer = document.getElementById("topnavLinksContainer");
+// topnavLinksContainer.style.paddingTop = (topnavHeight/5)+"px";
+var bannerSection = document.getElementById('bannerSection');
+// bannerSection.style.paddingTop = ((topnavHeight+20)+"px");
