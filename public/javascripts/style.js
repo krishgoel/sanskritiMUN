18997af2983203e0e83cd1e548f27a1fc0dfde06
Krish Skywalker <projectbeta@sanskritischool.edu.in>
2020-01-23 20:17:53 +0530

    Website v1.5

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -4,3 +4,36 @@ var topnavHeight = document.getElementById('topnav').offsetHeight;
 // topnavLinksContainer.style.paddingTop = (topnavHeight/5)+"px";
 var bannerSection = document.getElementById('bannerSection');
 // bannerSection.style.paddingTop = ((topnavHeight+20)+"px");
+
+// Countdown timer
+// Set the date we're counting down to
+var countDownDate = new Date("Apr22, 2020 08:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + " days " + hours + ": "
+  + minutes + " hours";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+// Committee card popups
